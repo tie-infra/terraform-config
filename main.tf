ceb3ae69148cadd0a4d5ed9ed6bab3b5e6d09750
Ivan Trubach <mr.trubach@icloud.com>
2021-07-16 02:55:04 +0300

    chore: nah, let’s try pulumi instead of terraform

--- main.tf
+++ main.tf
@@ -5,12 +5,18 @@ terraform {
       name = "terraform-config"
     }
   }
+
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
+  }
 }
 
-module "domain_wtf_tie" {
-  source = "./domain/wtf_tie"
-}
+#module "domain_wtf_tie" {
+#  source = "./domain/wtf_tie"
+#}
 
-module "domain_rip_tie" {
-  source = "./domain/rip_tie"
-}
+#module "domain_rip_tie" {
+#  source = "./domain/rip_tie"
+#}
